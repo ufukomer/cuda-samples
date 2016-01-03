99c938a7a5f9747498ae2e4c6effbed1f5742d47
ufukomer <ufukomer@hotmail.com>
2016-01-03 21:32:55 +0200

    MatrixMult modified

--- 04_MatrixMult/kernel.cu
+++ 04_MatrixMult/kernel.cu
@@ -1,3 +1,9 @@
+#pragma once
+#ifdef __INTELLISENSE__
+void __syncthreads();
+#endif
+
+
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
@@ -8,7 +14,7 @@
 
 #define BLOCK_DIM 16
 
-// Column & Row size
+// Row Size & Column Size
 const int N = 2;
 const int SIZE = N * N;
 
@@ -17,13 +23,13 @@ __global__ void matrixMult(int *c, int *a, int *b, int n)
 	int col = blockIdx.x * blockDim.x + threadIdx.x;
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
 
-	int k, sum = 0;
+	int sum = 0;
 
 	if (row < n && col < n)
 	{
-		for (k = 0; k < n; k++)
-			sum += a[row * n + k] * b[k * n + col];
-
+		for (int i = 0; i < n; ++i) 
+			sum += a[row * n + i] * b[i * n + col];
+			
 		c[row * n + col] = sum;
 	}
 }
@@ -32,7 +38,13 @@ int main()
 {
 	srand(time(NULL));
 
-	int a[N][N], b[N][N], *c;
+	int a[N][N] = { { 1, 2 },
+					{ 2, 1 } };
+
+	int	b[N][N] = { { 1, 2 },
+					{ 2, 1 } };
+
+	int *c;
 	int *dev_a, *dev_b, *dev_c;
 
 	c = (int *)malloc(SIZE * sizeof(int));
@@ -41,18 +53,18 @@ int main()
 	cudaMalloc((void **)&dev_b, SIZE * sizeof(int));
 	cudaMalloc((void **)&dev_c, SIZE * sizeof(int));
 
-	for (int row = 0; row < N; ++row)
+	/*for (int row = 0; row < N; ++row)
 		for (int col = 0; col < N; ++col)
 		{
 			a[row][col] = rand() % 10;
 			b[row][col] = rand() % 10;
 		}
-
+		*/
 	cudaMemcpy(dev_a, a, SIZE * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(dev_b, b, SIZE * sizeof(int), cudaMemcpyHostToDevice);
 
-	dim3 dimBlock(1, 1);
-	dim3 dimGrid(N, N);
+	dim3 dimBlock(N, N);
+	dim3 dimGrid((int)ceil(N / dimBlock.x), (int)ceil(N / dimBlock.y));
 
 	matrixMult <<< dimGrid, dimBlock >>>(dev_c, dev_a, dev_b, N);
 
